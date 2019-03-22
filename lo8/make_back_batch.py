247cc21c470440ff24bdfbaf318859fb1b6280f9
parkermac <p.maccready@gmail.com>
2019-03-22 15:27:29 -0700

    lo8
    edits to make less redundant information

--- lo8/make_back_batch.py
+++ lo8/make_back_batch.py
@@ -12,25 +12,27 @@ parser = argparse.ArgumentParser()
 
 # positional arguments
 parser.add_argument('-xp', '--exe_path', type=str, help="output path to dot.in file")
-parser.add_argument('-npn', '--np_num', type=int, help="number of cores to use")
+parser.add_argument('-np', '--np_num', type=int, help="number of cores to use")
+parser.add_argument('-N', '--cores_per_node', type=int, help="cores per node")
+parser.add_argument('-x', '--ex_name', type=str, help="executable name")
 
 args = parser.parse_args()
 
 EXE_PATH = args.exe_path
+NP_NUM = args.np_num
+CORES_PER_NODE = args.cores_per_node
+EX_NAME = args.ex_name
 print('Creating batch script for LiveOcean forecast run for:')
 print(EXE_PATH)
 
-NP_NUM = args.np_num
-
-#N_NODES = int(NP_NUM /28)
-N_NODES = int(NP_NUM /32)
+N_NODES = int(NP_NUM / CORES_PER_NODE)
 
 ## create lo_batch.sh - batch job script  ##########################
 
 f  = open('lo_back_BLANK.sh','r')
 f2 = open('lo_back_batch.sh','w')
 
-in_varlist = ['EXE_PATH', 'N_NODES']
+in_varlist = ['EXE_PATH', 'N_NODES', 'CORES_PER_NODE', 'EX_NAME']
 
 for line in f:
     for var in in_varlist:
