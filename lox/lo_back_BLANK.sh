5c2b3a35b614341668e55a2f109411b9473fe90c
parkermac <p.maccready@gmail.com>
2019-09-24 12:56:51 -0700

    lox
    new executable for the aestus3 grid

---
+++ lox/lo_back_BLANK.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+## Job Name
+#SBATCH --job-name=LiveOcean
+
+## Resources
+## Nodes
+#SBATCH --nodes=$N_NODES$
+## Tasks per node (Slurm assumes you want to run 28 tasks per node unless explicitly told otherwise)
+#SBATCH --ntasks-per-node=$CORES_PER_NODE$
+
+## Walltime 
+#SBATCH --time=14:00:00
+
+## Memory per node
+#SBATCH --mem=128G
+
+## Specify the working directory for this job
+#SBATCH --chdir=/gscratch/macc/parker/LiveOcean_roms/makefiles/$EX_NAME$
+
+module load icc_17-impi_2017
+module load netcdf_fortran+c_4.4.1.1-icc_17
+
+echo $EXE_PATH$
+
+mpirun ./oceanM $EXE_PATH$liveocean.in > $EXE_PATH$log.txt
+
